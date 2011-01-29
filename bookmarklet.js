a18a390bfd645cd0860f37bc9c973227116d95ca
Brian Carstensen <brian.carstensen@gmail.com>
2011-01-28 23:44:06 -0600

    Bookmarklet updated to allow full configuration

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,14 +1,14 @@
-javascript: void(function(scriptSrc, imgSrc) {
+javascript: void(function(scriptSrc, config) {
 	var script = document.createElement('script');
 	script.src = scriptSrc;
 	script.type = 'text/javascript';
 	script.addEventListener('load', function() {
 		if ('phantomLimb' in window) {
-			phantomLimb.init({src: imgSrc});
+			phantomLimb.init(config);
 		} else {
 			console.error('Phantom Limb could not be loaded');
 		}
 	}, false);
 	
 	document.head.appendChild(script);
-}('https://github.com/brian-c/phantom-limb/raw/master/phantomLimb.js', 'https://github.com/brian-c/phantom-limb/raw/master/limb-black.png'));
+}('https://github.com/brian-c/phantom-limb/raw/master/phantomLimb.js', {src: 'https://github.com/brian-c/phantom-limb/raw/master/limb-black.png', lefty: false}));
