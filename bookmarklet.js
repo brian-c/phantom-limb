cf192f00911e8686f9db198cf686e64818ada990
Brian Carstensen <brian.carstensen@gmail.com>
2013-11-08 21:22:05 -0600

    Remove bookmarklet

--- bookmarklet.js
+++
@@ -1,6 +0,0 @@
-javascript:void(function(commit) {
-	var scriptTag = document.createElement('script');
-	scriptTag.type = 'text/javascript';
-	scriptTag.src = 'https://raw.github.com/brian-c/phantom-limb/' + commit + '/phantom-limb.js';
-	document.body.appendChild(scriptTag);
-}('v2.0.1'));
